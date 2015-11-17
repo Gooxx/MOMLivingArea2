10c612daa715f607f772eadbf79770a8b99a105e
Gooxx <goooo@aaa.local>
2015-11-17 11:39:29 +0800

    Initial Commit

---
+++ MOMLivingArea/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MOMLivingArea
+//
+//  Created by goooo on 15/11/17.
+//  Copyright © 2015年 mom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
